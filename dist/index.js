78ea05c7c08f41f179274f76e9f38220b549cfab
Matthew Lothian <lothian.studios@gmail.com>
2017-11-07 12:26:08 -0200

    removed dist folder from version control add added package-lock.json

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { NgxCarouselModule } from './src/ngx-carousel.module';
-export { NgxCarousel, NgxCarouselStore } from './src/ngx-carousel/ngx-carousel.interface';
-//# sourceMappingURL=index.js.map
